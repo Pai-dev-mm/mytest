20cdebc91acc1d67e2217c4e40b49534620234b8
Pai thet <leontinus866@gmail.com>
2024-02-02 01:09:54 +0630

    test branch first commit

--- server.ts
+++ server.ts
@@ -6,6 +6,8 @@ const PORT = 8800;
 const server = http.createServer((req, res) => {
   const showData = { id: 1, name: "Mg Mg", address: "SouthOkkalap Township" };
   res.writeHead(200, "OK Nrr srr", { "Content-Type": "application/json" });
+  console.log("This is testing git hub collobrate");
+
   res.write(JSON.stringify(showData));
   res.end();
 });
